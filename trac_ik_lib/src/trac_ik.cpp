d08d8a4e9a1acdae28a9bcd8da16238e0bc79a42
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2026-06-01 15:40:23 +0200

    [jazzy] jacsolver needs locking too (#5)
    * No need for stream logging
    
    * jacsolver needs locking too
    
    * More robust code

--- trac_ik_lib/src/trac_ik.cpp
+++ trac_ik_lib/src/trac_ik.cpp
@@ -63,7 +63,7 @@ namespace TRAC_IK
 
   robot_model.initString(xml_string);
 
-  RCLCPP_DEBUG_STREAM(nh_->get_logger(), "Reading joints and links from URDF");
+  RCLCPP_DEBUG(nh_->get_logger(), "Reading joints and links from URDF");
 
   KDL::Tree tree;
 
@@ -244,7 +244,6 @@ bool TRAC_IK::runSolver(T1& solver, T2& other_solver,
         solutions.push_back(q_out);
         uint curr_size = solutions.size();
         errors.resize(curr_size);
-        mtx_.unlock();
         double err, penalty, manip_value;
         switch (solvetype)
         {
@@ -267,7 +266,6 @@ bool TRAC_IK::runSolver(T1& solver, T2& other_solver,
           err = TRAC_IK::JointErr(q_init, q_out);
           break;
         }
-        mtx_.lock();
         errors[curr_size - 1] = std::make_pair(err, curr_size - 1);
       }
       mtx_.unlock();
@@ -416,6 +414,7 @@ int TRAC_IK::CartToJnt(const KDL::JntArray &q_init, const KDL::Frame &p_in, KDL:
   nl_solver->reset();
   iksolver->reset();
 
+  // No lock as no threading yet
   solutions.clear();
   errors.clear();
 
@@ -424,9 +423,12 @@ int TRAC_IK::CartToJnt(const KDL::JntArray &q_init, const KDL::Frame &p_in, KDL:
   task1 = std::thread(&TRAC_IK::runKDL, this, q_init, p_in);
   task2 = std::thread(&TRAC_IK::runNLOPT, this, q_init, p_in);
 
-  task1.join();
-  task2.join();
+  if (task1.joinable())
+      task1.join();
+  if (task2.joinable())
+      task2.join();
 
+  // No lock as no threading anymore
   if (solutions.empty())
   {
     q_out = q_init;
@@ -453,6 +455,11 @@ int TRAC_IK::CartToJnt(const KDL::JntArray &q_init, const KDL::Frame &p_in, KDL:
 
 TRAC_IK::~TRAC_IK()
 {
+  if (initialized)
+  {
+    iksolver->abort();
+    nl_solver->abort();
+  }
   if (task1.joinable())
     task1.join();
   if (task2.joinable())
